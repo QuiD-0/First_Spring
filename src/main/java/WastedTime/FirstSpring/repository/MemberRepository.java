fc9255d54a38a42eecec84614819c5b687d512a4
QuiD <wodnd101@naver.com>
2021-10-01 20:05:54 +0900

    member

---
+++ src/main/java/WastedTime/FirstSpring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package WastedTime.FirstSpring.repository;
+
+import WastedTime.FirstSpring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
