e8368b171889b9365226323805269de9bd5a1667
QuiD <wodnd101@naver.com>
2021-10-04 22:27:34 +0900

    Test member

--- src/main/java/WastedTime/FirstSpring/controller/HelloController.java
+++ src/main/java/WastedTime/FirstSpring/controller/HelloController.java
@@ -1,4 +1,4 @@
-package WastedTime.FirstSpring.helloController;
+package WastedTime.FirstSpring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ src/main/java/WastedTime/FirstSpring/service/MemberService.java
@@ -0,0 +1,40 @@
+package WastedTime.FirstSpring.service;
+
+import WastedTime.FirstSpring.domain.Member;
+import WastedTime.FirstSpring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member) {
+        ValidateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void ValidateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
